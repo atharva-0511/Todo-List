fbafbcbf3dba48dfcdc1fc03e86612c3d374a8dd
Atharva Sattigeri <atharvasattigeri9933@gmail.com>
2026-09-25 11:34:37 +0530

    Added 'Add Task' feature

--- todo-list/app/page.js
+++ todo-list/app/page.js
@@ -2,16 +2,32 @@
 import React, { use, useState } from 'react'
 
 const page = () => {
-  const [task, settask] = useState("");
-  const [desc, setdesc] = useState("");
+  const [task, setTask] = useState("");
+  const [desc, setDesc] = useState("");
+  const [mainTask, setMainTask] = useState([]);
 
   const submitHandler = (e)=>{
     e.preventDefault()
-    console.log(task)
-    console.log(desc)
-    settask("")
-    setdesc("")
+    setMainTask([...mainTask, { task, desc }]);
+    setTask("")
+    setDesc("")
+    console.log(mainTask)
   }
+
+  let renderTask = <h2>No Task</h2>
+
+  if(mainTask.length>0){
+    renderTask = mainTask.map((t,i) => {
+      return (
+        <li key={i}>
+          <div className='flex justify-between mb-5'>
+            <h5 className='text-2xl font-semibold'>{t.task}</h5>
+            <h6 className='text-xl font-semibold'>{t.desc}</h6>
+          </div>
+        </li>
+      );
+    });
+  };
 
   return (
     <>
@@ -21,18 +37,25 @@ const page = () => {
         placeholder='Enter Task'
         value = {task}
         onChange={(e)=>{
-          settask(e.target.value)
+          setTask(e.target.value)
         }}
       />
       <input type='text' className='text-2xl border-4 rounded-2xl m-5 p-5'
         placeholder='Enter Description'
         value = {desc}
         onChange={(e)=>{
-          setdesc(e.target.value)
+          setDesc(e.target.value)
         }}
       />
       <button className='bg-emerald-50 text-black border-2 rounded-2xl p-2'>Add Task</button>
     </form>
+
+    <hr/>
+
+    <div className='p-8 bg-slate-200'>
+      <ul>{renderTask}</ul>
+    </div>
+
     </>
   )
 }
